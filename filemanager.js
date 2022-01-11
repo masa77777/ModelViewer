b5a555ff0bb79dd453cdc3ba6622daef70d5852b
masa77777 <44291634+masa77777@users.noreply.github.com>
2022-01-11 17:15:07 +0900

    update

---
+++ filemanager.js
@@ -0,0 +1,28 @@
+let post = document.getElementById('post');
+post.addEventListener('click', execUpload);
+
+function execUpload() {
+    let upload = document.getElementById('upload');
+    let file1 = upload.files[0];
+    // console.log(file1);
+
+    let formData = new FormData();
+    formData.append('modelfile', file1);
+    for (let value of formData.entries()) {
+        console.log(value);
+    }
+
+    let xmlHttp = new XMLHttpRequest();
+    // console.log("1: " + xmlHttp.readyState);
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            alert(xmlHttp.responseText);
+        }
+    };
+    // console.log("2: " + xmlHttp.readyState);
+    xmlHttp.open('POST', 'getForm.php', true);
+    // console.log("3: " + xmlHttp.readyState);
+    // xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xmlHttp.send(formData);
+    // console.log("5: " + xmlHttp.readyState);
+}
--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <title>Document</title>
     <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js">
     </script>
+    <script src="filemanager.js" defer></script>
     <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-    <div class="wrapper">
+    <!-- <div class="wrapper">
         <div><a href=""><img src="./models/popochan.jpg" alt=""></a></div>
         <div>
             <model-viewer src="./models/applefuji_preview.glb" ios-src="./models/applefuji_preview.usdz" alt="3D Model"
@@ -37,14 +38,37 @@
         <div>Six</div>
         <div>Seven</div>
         <div>Eight</div>
-    </div>
+    </div> -->
+
     <?php
-    $images = glob('./models/*jpg');
-    foreach($images as $v) {
-        echo "<img src='", $v, "' alt='' loading='lazy'>";
-    };
+    // $images = glob('./models/*jpg');
+    // foreach($images as $v) {
+    //     echo "<img src='", $v, "' alt='' loading='lazy'>¥n";
+    // }
     ?>
 
+    <div class="wrapper">
+    <?php
+    $glbFile = glob('./models/*glb');
+
+    foreach ($glbFile as $v) {
+        $usdzFilename = pathinfo($v, PATHINFO_FILENAME);
+        $usdzFile = "./models/" . $usdzFilename . ".usdz";
+
+        echo "<div>".PHP_EOL;
+        echo "<model-viewer src='", $v, "' ios-src='", $usdzFile, "' alt='3D Model' auto-rotate
+        camera-controls ar ar-modes='quick-look scene-viewer webxr' shadow-intensity='1'>".PHP_EOL;
+        echo "</model-viewer>".PHP_EOL;
+        echo "</div>".PHP_EOL;
+    }
+    ?>
+    </div>
+
+    <!-- <div class="control">
+    <form id="uploadForm" enctype="multipart/form-data">
+      <input type="file" id="upload" name="modelfile" accept=".glb,.usdz">
+      <button type="button" id="post">Upload</button>
+    </form> -->
 
 </body>
 
